fa311f6fbb92c0ea247801be043dcf0048b121ed
Aissatou T <114699702+AITHIOMB@users.noreply.github.com>
2025-07-27 07:14:45 -0700

    Update feelingsick.py

--- feelingsick mlh hacking/feelingsick.py
+++ feelingsick mlh hacking/feelingsick.py
@@ -1,6 +1,6 @@
-
+G
 from google import genai
-client = genai.Client(api_key="AIzaSyDTzd5cIjS2ExSR80UFtgNfUjp7gro3v0U")
+client = genai.Client(api_key="gemini key")
 chat = client.chats.create(model="gemini-2.5-flash")
 response = chat.send_message("Symptom described: ...")
 print(response.text)
